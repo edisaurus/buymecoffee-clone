b3422593f05268880db7bd2f5bce9a12ec576926
edisaurus <edswork.ece@gmail.com>
2021-08-09 19:01:49 +0800

    create post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_065545) do
+ActiveRecord::Schema.define(version: 2021_08_09_105727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_08_09_065545) do
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "follower_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
